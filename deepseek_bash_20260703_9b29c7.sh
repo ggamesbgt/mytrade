06e3500b50769c4086f034a5ae66db8214c4b32f
ggamesbgt <gerry@esaunggul.ac.id>
2026-07-04 01:21:46 +0700

    Add files via upload

---
+++ deepseek_bash_20260703_9b29c7.sh
@@ -0,0 +1,11 @@
+# Clone repository
+git clone https://github.com/ggamesbgt/mytrade.git
+cd mytrade
+
+# Buat file index.html (paste kode di atas)
+# atau download dan simpan
+
+# Commit dan push
+git add index.html
+git commit -m "Deploy Wyckoff simulation"
+git push origin main
